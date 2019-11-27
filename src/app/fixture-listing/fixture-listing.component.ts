349a8350c16afa2de013b58354cb06ea54b4b789
Craig Ivemy <craigivemy@gmail.com>
2019-11-27 22:33:06 +0000

    Nearly working: fixtures by week

--- src/app/fixture-listing/fixture-listing.component.ts
+++ src/app/fixture-listing/fixture-listing.component.ts
@@ -6,7 +6,7 @@ import {Fixture} from '../models/fixture';
 import {selectCurrentSeasonId} from '../season/season.selectors';
 import {loadFixtures} from '../fixture/fixture.actions';
 import {selectAllFixtures} from '../fixture/fixture.selectors';
-import {groupBy, map, mergeMap, skipWhile, tap, toArray} from 'rxjs/operators';
+import {filter, groupBy, map, mergeMap, skipWhile, take, tap, toArray} from 'rxjs/operators';
 import {FixtureService} from '../fixture.service';
 
 @Component({
@@ -32,14 +32,16 @@ this.store.pipe(
   //   tap(res => console.log(res))
   // ).subscribe(fixtures => this.fixtures$ = fixtures);
       this.store.dispatch(loadFixtures({seasonId}));
-      this.store.pipe(
+      this.fixtures$ = this.store.pipe(
         select(selectAllFixtures),
+        filter(arr => arr.length > 0),
+        take(1),
         mergeMap(fixtures => fixtures),
         groupBy(fixture => fixture.matchDate),
-        tap(res => console.log(res)),
         mergeMap(group => group.pipe(toArray())),
+        toArray(),
         tap(res => console.log(res))
-      ).subscribe(fixtures => this.fixtures$ = fixtures);
+      );
     });
   }
 
